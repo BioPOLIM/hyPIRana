312dc1cdf70eac43f5f8ef5e329e720fa98c3e69
Maryam Ali <117171197+Maryam3Ali@users.noreply.github.com>
2026-01-08 14:48:09 +0100

    Update hyPiRana.py

--- hyPiRana.py
+++ hyPiRana.py
@@ -227,7 +227,113 @@ if set_PCA > 1:
             ax.legend_ = None
             plt.title('Data '+ str(i+1)+'PC'+str(icomp+1))
             my_fig.tight_layout()
+
+
+#%% Heirarchical Clustering Analysis HCA
+
+# Create a dendrogram for the ccombined data
+# by calculating the distances between the spectra using Eclidean distance & ward method 
+
+clustering = linkage(NORMSpc, method = "ward", metric="euclidean")
+plt.figure()
+dendrogram(clustering, color_threshold=0.07, above_threshold_color='grey')
+plt.title('Dendrogram of Clusters')
+plt.xlabel('Spectra')
+plt.ylabel('Distances')
+# Optional: plotting a line at where the user can estimate number of desired clusters 
+plt.axhline(y = 0.1, color = 'black', linestyle = 'solid'  )
+plt.show()
+
+#%% Agglomerative clustering
+# Here, the same steps are done to calculate the distances and generate clusters
+# but the algorithm stops at a chosen number of clusters
+
+# based on the line the user plot abive, number of desired clusters should be chosen here
+# supervised clustering
+
+n_clusters = 3
+
+clustering_model = AgglomerativeClustering(n_clusters= n_clusters, metric='euclidean')
+clustering_model.fit_predict(NORMSpc)
+
+data_labels = clustering_model.labels_
+
+#to check number of spectra per cluster
+for i in range(n_clusters):
+    number_spc = NORMSpc[data_labels == i].shape
+    print('Cluster ' + str(i+1) + '  has:  ' + str(number_spc[0]))
+    
+#%% plotting the mean spectra for each cluster
+
+
+hca_means = []
+hca_means_sm = [] 
+
+
+plt.figure()
+for i in range (n_clusters):
+    cluster_mean = np.mean(NORMSpc[data_labels == i], axis = 0)
+    cluster_mean_sm = signal.savgol_filter((cluster_mean), window_length=11, polyorder=2, mode="nearest")
+    plt.plot(Data[0]['wavelength'], cluster_mean_sm.T, label = 'cluster' + str(i+1))
+    plt.legend()
+    hca_means.append(cluster_mean)
+    hca_means_sm.append(cluster_mean_sm)
+    
+plt.gca().invert_xaxis()
+plt.title(' HCA Mean Spectra')
+plt.xlabel('Wavenumber (cm\u207b1)')
+plt.ylabel('intensity (normalized)')
+plt.show()
+
+#Exporting mean spectra
+#np.savetxt('HCA_means_smoothed.txt', hca_means_sm, delimiter='\t')
+
+#%% cluster maps
+# created using non negative factorization NNF
+
+H, W, n =  non_negative_factorization(np.abs(NORMSpc), n_components = n_clusters, init='custom',
+                                      update_H=False, H=np.abs(hca_means), max_iter = 10000)
+
+pix = Img_pix[0][0]
+b = 0
+res = H.__copy__().T
+maps_hca = []
+
+for ic, r in enumerate(res):
+    my_fig= plt.figure()
+    ax = plt.subplot(111)
+    plt.colorbar(ax.imshow(np.reshape(r, (xpix, ypix)), cmap = 'gray', vmin = 0, vmax = 1))  
+    plt.title(' cluster ' + str(ic+1) + ' map')
+    plt.yticks([])
+    plt.xticks([])
+    plt.tight_layout()
+    maps_hca.append(np.reshape(r, (xpix, ypix)))
+
+
+
+#Individual plots per each dataset
+
+pix = Img_pix[0][0]
+b = 0
+
+if set_PCA > 1:   
+    maps_hca = np.array(maps_hca)
+    for d in range(set_PCA):
+        for n_c in range(n_clusters):            
+            my_fig = plt.figure()
+            ax = plt.subplot(111)
+            plt.colorbar(ax.imshow(maps_hca[n_c, b:pix, :], cmap = 'gray', vmin = 0, vmax = 1))
+            ax.set_xlabel('x scan ['+Data[0]['XPhysUnit']+']')
+            ax.set_ylabel('y scan ['+Data[0]['XPhysUnit']+']')
+            ax.legend_ = None
+            plt.title('Data '+ str(d+1)+' cluster ' + str(n_c+1))
+            my_fig.tight_layout()
         
+        
+        b = b + Img_pix[d][0]
+        pix = pix + Img_pix[d][0]
+            
+
         
         b = b + Img_pix[i][0]
         pix = pix + Img_pix[i][0]
